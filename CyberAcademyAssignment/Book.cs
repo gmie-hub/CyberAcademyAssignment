4d3b715a2fbebc8053d55b1a4f3a479adbfd3ffc
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-05 13:37:13 -0700

    Add project files.

---
+++ CyberAcademyAssignment/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberAcademyAssignment
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public string ISBN;
+
+        public void ReadableBooks()
+        {
+            Console.WriteLine("This book is available for reading");
+        }
+        
+        public void ShelvableBooks()
+        {
+            Console.WriteLine("This book should be shelved");
+        }
+    }
+}
---
+++ CyberAcademyAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CyberAcademyAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+
+            book.Title = "Holiness";
+            book.Author = "J.C. Ryle";
+            book.ISBN = "978-92-95055-02-5";
+
+            book.ReadableBooks();
+            book.ShelvableBooks();
+            Console.WriteLine("Title: {0} \nAuthor: {1} \nISBN: {2}",
+                book.Title, book.Author, book.ISBN);
+        }
+    }
+}
