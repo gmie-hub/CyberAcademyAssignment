cb9e179d7d00cb51629a0613a4391fbb5bc673ea
Oluwafolajimi Olaosebikan <folagmie16@gmail.com>
2022-04-06 01:10:38 -0700

    Assignment 06-04-2022

---
+++ CyberAssignment2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CyberAssignment2
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Car car = new Car();
+            car.ViewModelId("Cross tour");
+            car.GetManufacturerId();
+
+            Truck truck = new Truck();
+            truck.ViewModelId("Hiace");
+            truck.GetManufacturerId();  
+
+            Motorcycle motorcycle = new Motorcycle();
+            motorcycle.ViewModelId("scooter");
+            motorcycle.GetManufacturerId();
+        }
+    }
+}
---
+++ CyberAssignment2/Vehicle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberAssignment2
+{
+    public class Vehicle
+    {
+        public string manufacturerId = "Honda";
+        
+        public void ViewModelId(string modelId)
+        {
+            Console.WriteLine("This is a {0}", modelId);
+        }
+        
+    }
+
+    public class Car : Vehicle
+    {
+        public string modelId = "Car";
+
+        public void GetManufacturerId()
+        {
+            Console.WriteLine("Model: {0}", manufacturerId);
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+        public string modelId = "Truck";
+
+        public void GetManufacturerId()
+        {
+            Console.WriteLine("Model: {0}", manufacturerId);
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public string modelId = "Motorcycle";
+
+        public void GetManufacturerId()
+        {
+            Console.WriteLine("Model: {0}", manufacturerId);
+        }
+    }
+
+}
